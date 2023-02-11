d7a9bc573353af4960331a2e8b71b1fc315c6ad2
Mikaela Szekely <mikaela.szekely@qyriad.me>
2023-02-10 18:30:09 -0700

    descriptors: add config/iface/endpoint descriptors

--- src/descriptor.rs
+++ src/descriptor.rs
@@ -24,3 +24,53 @@ pub struct DeviceDescriptor {
     pub iSerialNumber: u8,
     pub bNumConfigurations: u8,
 }
+
+// TODO: should this be Copy?
+#[derive(Copy, Debug, Clone, PartialEq, Hash)]
+#[allow(non_snake_case)]
+#[binrw]
+#[brw(little)]
+pub struct ConfigurationDescriptor {
+    pub bLength: u8,
+    /// Constant: 2.
+    pub bDescriptorType: u8,
+    pub wTotalLength: u16,
+    pub bNumInterfaces: u8,
+    pub bConfigurationValue: u8,
+    pub iConfiguration: u8,
+    pub bmAttributes: u8,
+    pub bMaxPower: u8,
+}
+
+// TODO: should this be Copy?
+#[derive(Copy, Debug, Clone, PartialEq, Hash)]
+#[allow(non_snake_case)]
+#[binrw]
+#[brw(little)]
+pub struct InterfaceDescriptor {
+    pub bLength: u8,
+    /// Constant: 4
+    pub bDescriptorType: u8,
+    pub bInterfaceNumber: u8,
+    pub bAlternateSetting: u8,
+    pub bNumEndpoints: u8,
+    pub bInterfaceClass: u8,
+    pub bInterfaceSubClass: u8,
+    pub bInterfaceProtocol: u8,
+    pub iInterface: u8,
+}
+
+// TODO: should this be Copy?
+#[derive(Copy, Debug, Clone, PartialEq, Hash)]
+#[allow(non_snake_case)]
+#[binrw]
+#[brw(little)]
+pub struct EndpointDescriptor {
+    pub bLength: u8,
+    /// Constant: 5.
+    pub bDescriptorType: u8,
+    pub bEndpointAddress: u8,
+    pub bmAttributes: u8,
+    pub wMaxPacketSize: u16,
+    pub bInterval: u8,
+}
